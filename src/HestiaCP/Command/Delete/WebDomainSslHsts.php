7da821ef4d934fb42379980b7cad9515acd3fda3
ponasromas <rms200x@gmail.com>
2023-08-31 13:19:34 +0300

    hsts control

---
+++ src/HestiaCP/Command/Delete/WebDomainSslHsts.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace HestiaCP\Command\Delete;
+
+use HestiaCP\Command\ProcessCommand;
+
+class WebDomainSslHsts extends ProcessCommand {
+
+	/** @var string */
+	private $user;
+
+	/** @var string */
+	private $domain;
+
+	public function __construct(string $user, string $domain) {
+		$this->user = $user;
+		$this->domain = $domain;
+	}
+
+	public function getName(): string {
+		return 'v-delete-web-domain-ssl-hsts';
+	}
+
+	public function getRequestParams(): array {
+		return [
+			self::ARG_1 => $this->user,
+			self::ARG_2 => $this->domain
+		];
+	}
+}
